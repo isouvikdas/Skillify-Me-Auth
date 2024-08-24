3e2ed5ba929284047f0edd2bb06b9cfc5e539066
isouvikdas <developersouvik02@gmail.com>
2024-08-24 22:56:10 +0530

    something went wrong trying to fix

--- src/main/java/com/skillifyme/auth/Skillify/Me/Auth/utils/GenerateOTP.java
+++ src/main/java/com/skillifyme/auth/Skillify/Me/Auth/utils/GenerateOTP.java
@@ -7,7 +7,6 @@ import java.util.Random;
 
 @Component
 public class GenerateOTP {
-
     public String generateOtp() {
         Random random = new Random();
         return String.valueOf(random.nextInt(1000000));
